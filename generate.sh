24df191ead8c9a5e547d2e2433e801c55235640a
Vlad Tokarev <vlad.tokarev.94@gmail.com>
2021-04-26 13:13:22 +0300

    Change batch example from feature branch to tag (#110)

--- generate.sh
+++ generate.sh
@@ -1,8 +1,22 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 set -e
 SRC_DIR=source
 OUT_DIR=out
 SKIP_PDF=false
+
+function setup_openapi_ref() {
+  echo "Fetching  ${SHORT_VERSION} branch in odahu/odahu-flow repository for open api spec"
+  set +e
+  http_code=$(wget -S -O "${SRC_DIR}/odahu-core-openapi.yaml" https://raw.githubusercontent.com/odahu/odahu-flow/${SHORT_VERSION}/packages/operator/docs/swagger.yaml 2>&1 | grep "HTTP/" | awk '{print $2}')
+  echo "Fetching ${SHORT_VERSION} branch HTTP response code = ${http_code}"
+  # Use develop branch in odahu/odahu-flow repo to fetch API specification (OpenAPI yaml config)
+  # We consider that this is feature branch in docs repo and there is no corresponding branch in odahu-flow repo
+  if [[ $http_code -ne 200 ]]; then
+      echo "Unable to fetch ${SHORT_VERSION} branch in odahu/odahu-flow repository. Fetching develop branch..."
+      wget -O "${SRC_DIR}/odahu-core-openapi.yaml"  https://raw.githubusercontent.com/odahu/odahu-flow/develop/packages/operator/docs/swagger.yaml
+  fi
+  set -e
+}
 
 while [ "$#" -gt 0 ]; do
     sleep 1
@@ -21,8 +35,7 @@ done
 
 rm -rf "${OUT_DIR}"
 
-echo "Fetching swagger spec for ODAHU API"
-wget -O "${SRC_DIR}/odahu-core-openapi.yaml"  https://raw.githubusercontent.com/odahu/odahu-flow/feat/batch-test/packages/operator/docs/swagger.yaml
+setup_openapi_ref
 
 echo "Building of HTML docs"
 sphinx-build -b html "${SRC_DIR}" "${OUT_DIR}"
--- source/ref_batch.rst
+++ source/ref_batch.rst
@@ -18,7 +18,7 @@ Currently Batch Inference supports the following ways to delivery data for forec
 
 In future we consider to add ability to process data directly from Kafka topic and other async data sources.
 
-Please also take a look at `example <https://github.com/odahu/odahu-examples/tree/feat/batch-inference-example/batch-inference>`_.
+Please also take a look at `example <https://github.com/odahu/odahu-examples/tree/1.5.0-rc4/batch-inference>`_.
 
 
 
@@ -120,4 +120,3 @@ This section helps with deeper understanding of underlying mechanisms.
   8. Log Model Output to feedback storage
   9. Upload data from ``$ODAHU_MODEL_OUTPUT`` to ``[].spec.outputDestination.path``
 
-.. image:: img/batch.png
